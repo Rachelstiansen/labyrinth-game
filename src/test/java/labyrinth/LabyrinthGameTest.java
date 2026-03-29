6b24b884c02feb15c577e370e695ce8c136a049b
Rachel Stiansen <rachelks@ntnu.no>
2026-03-29 20:43:26 +0200

    progress on test

--- src/test/java/labyrinth/LabyrinthGameTest.java
+++ src/test/java/labyrinth/LabyrinthGameTest.java
@@ -1,5 +1,15 @@
 package labyrinth;
 
+import org.junit.jupiter.api.Test;
+
 public class LabyrinthGameTest {
-    
+    @Test
+    private void testCannotMoveOutsideGrid() {
+
+    }
+
+    @Test
+    private void testGameNotOverAtStart() {
+        
+    }
 }
